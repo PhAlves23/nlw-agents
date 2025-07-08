29d39be408b87e4aff6aaac1dddff4cdb0fc9ff4
PhAlves23 <ph23.alves@gmail.com>
2025-07-07 23:51:24 -0300

    chore: config routes structure

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
+const queryClient = new QueryClient()
 
 export function App() {
  return (
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<CreateRoom/>}/>
+      <Route path="/room/:roomId" element={<Room/>}/> 
+    </Routes>
+  </BrowserRouter>
+  </QueryClientProvider>
  )
 }
 
